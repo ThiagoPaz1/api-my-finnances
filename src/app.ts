0f841d139c559918a1a6ce9aff8d042fc53b6a4b
Thiago da Paz <thiagopazjunior@gmail.com>
2022-11-28 23:06:03 -0300

    feat: Criado nova configuração do projeto.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { AppDataSource } from "./data-source"
+import cors from 'cors'
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+AppDataSource.initialize()
+.then(() => console.log('Iniciado banco de dados!'))
+.catch(error => console.log(error))
+
+app.listen(3000)
