69eaf36c1019a3c9a7fa6790810e421a6b62fbe7
Thiago da Paz <thiagopazjunior@gmail.com>
2022-12-05 20:22:18 -0300

    refactor: Adjusted in routes.

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,6 +1,7 @@
-import { Router } from 'express'
-import { userController } from '../modules/user/controllers'
-import { validationFields } from '../modules/user/controllers/middlewares/validationFields'
+import { Router } from "express"
+
+import { userController } from "../modules/user/controllers"
+import { validationFields } from "../modules/user/controllers/middlewares/validationFields"
 
 const userRouter = Router()
 
